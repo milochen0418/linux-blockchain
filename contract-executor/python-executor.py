fe34b28c1e50fbae6547968c8b6c29b7a5b51730
Milo Chen <milo_chen@mail2000.com.tw>
2019-08-29 16:04:02 +0800

    Update

--- contract-executor/python-executor.py
+++ contract-executor/python-executor.py
@@ -1,6 +1,6 @@
 # This is python contract-executor
 import os
 def Execute(filename):
-    execfile(filename)
+    exec(compile(open(filename, "rb").read(), filename, 'exec'), globals, locals)
 
-Execute('test_smart_contarct.py')
+Execute('./test_smart_contract.py')
