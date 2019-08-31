bedf655d0c403086bd4458aa72783f4f6793340c
Milo Chen <milo_chen@mail2000.com.tw>
2019-08-31 23:07:32 +0800

    update

--- contract-executor/python-executor.py
+++ contract-executor/python-executor.py
@@ -1,21 +1,13 @@
 # This is python contract-executor
 import os
 def Execute(filename):
-    #exec(compile(open(filename, "rb").read(), filename, 'exec'), globals, locals)
-    #exec(compile(open(filename, "rb").read(), '<string>', 'exec')
-    #exec(compile('print("Hello")'), '<string>', 'exec')
+    readcode = open(filename,"rb").read()
+    execCodeObject = compile(readcode, '<string>', 'exec')
+    executeCodeBlock = exec(execCodeObject)
     pass
-#Execute('./test_smart_contract.py')
 
+Execute('./test_smart_contract.py')
+Execute('./test_smart_contract.py')
+exit(0)
 
-execCodeObject = compile('a = 8; a = a + 10; print(a)', '<string>', 'exec')
-executeCodeBlock = exec(execCodeObject)
-
-filename = 'test_smart_contract.py'
-
-QQ = open(filename,"rb").read()
-print(QQ)
-
-execCodeObject = compile(QQ, '<string>', 'exec')
-executeCodeBlock = exec(execCodeObject)
 
