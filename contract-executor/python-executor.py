34093ad3c2d302dbaac5ba1766da0f890dc9f52f
Milo Chen <milo_chen@mail2000.com.tw>
2019-08-31 22:32:16 +0800

    basic thing for POC smart contract executor

--- contract-executor/python-executor.py
+++ contract-executor/python-executor.py
@@ -1,6 +1,21 @@
 # This is python contract-executor
 import os
 def Execute(filename):
-    exec(compile(open(filename, "rb").read(), filename, 'exec'), globals, locals)
+    #exec(compile(open(filename, "rb").read(), filename, 'exec'), globals, locals)
+    #exec(compile(open(filename, "rb").read(), '<string>', 'exec')
+    #exec(compile('print("Hello")'), '<string>', 'exec')
+    pass
+#Execute('./test_smart_contract.py')
 
-Execute('./test_smart_contract.py')
+
+execCodeObject = compile('a = 8; a = a + 10; print(a)', '<string>', 'exec')
+executeCodeBlock = exec(execCodeObject)
+
+filename = 'test_smart_contract.py'
+
+QQ = open(filename,"rb").read()
+print(QQ)
+
+execCodeObject = compile(QQ, '<string>', 'exec')
+executeCodeBlock = exec(execCodeObject)
+
