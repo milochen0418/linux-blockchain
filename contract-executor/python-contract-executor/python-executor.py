79c93b393a3e1fb412635d3ff49bce0fa6bcb311
Milo Chen <milo_chen@mail2000.com.tw>
2019-09-02 15:49:55 +0000

    update load to test

--- contract-executor/python-contract-executor/python-executor.py
+++ contract-executor/python-contract-executor/python-executor.py
@@ -15,21 +15,32 @@ def python_object_load(filename):
     import os
     import time
     import pickle
-    file_r = open(filename, "rb")
-    obj2 = pickle.load(file_r)
-    file_r.close()
+	 try:
+	     file_r = open(filename, "rb")
+	     obj2 = pickle.load(file_r)
+	     file_r.close()
+	 except:
+		  file_r.close()
+		  return None
     return obj2
-storage = None
 
 def make_empty_storage():
     storage = {}
     python_object_dump(storage, 'storage.bytes')
+    return storage
     pass
 
 def Execute(filename):
+	
+	 storage = python_object_load(filename)
+    if storage == None:
+        storage = make_empty_storage()
+
     readcode = open(filename,"rb").read()
     execCodeObject = compile(readcode, '<string>', 'exec')
     executeCodeBlock = exec(execCodeObject)
+
+    python_object_dump(storage, 'storage.bytes')
     pass
 
 
