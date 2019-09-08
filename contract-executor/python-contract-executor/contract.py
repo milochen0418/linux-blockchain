df4096f6754ec3113942405d48506b1dca5b1d88
Milo Chen <milo_chen@mail2000.com.tw>
2019-09-09 00:58:02 +0800

    Add more test command examples  and add corrosponding function in smart contract

--- contract-executor/python-contract-executor/contract.py
+++ contract-executor/python-contract-executor/contract.py
@@ -2,6 +2,7 @@ class SmartContract:
     def __init__(self, storage):
         self.storage = storage
         pass
+
     def do_function_void(self):
         print('do_function()')
         if 'cnt' not in self.storage.keys():
@@ -10,10 +11,18 @@ class SmartContract:
             self.storage['cnt'] += 1
         # show storage data
         print(self.storage)
+
     def do_function_one_arg(self,arg1):
         print('invoke do_function_one_arg({})'.format( arg1))
+
     def func_with_args(self, argNum, argStr):
         print('argNum = ', argNum)
         print('argStr = ', argStr)
+
+    def storage_setter(self, KeyStr, Value):
+        self.storage[KeyStr] = Value
+        print('set done')
+    def storage_getter(self, KeyStr):
+        print('get ', self.storage[KeyStr])
 
 
--- contract-executor/python-contract-executor/python-executor.py
+++ contract-executor/python-contract-executor/python-executor.py
@@ -108,9 +108,14 @@ if __name__=="__main__":
         run_test_case()
     elif len(sys.argv) == 2:
         run_contract_by_jsonrpc(sys.argv[1])
+        # You can try the test command like the following
         #python python-executor.py '{"func":"do_function_void" }'
         #python python-executor.py '{"func":"do_function_one_arg", "arg1":"Hello World" }'
         #python python-executor.py '{"func":"func_with_args", "argNum":345, "argStr":"MyString"}'
+        #python python-executor.py '{"func":"storage_setter", "KeyStr":"OmgKey", "Value":3939889}'
+        #python python-executor.py '{"func":"storage_getter", "KeyStr":"OmgKey"}'
+        #python python-executor.py '{"func":"storage_setter", "KeyStr":"OmgKey", "Value":"Milo DApp"}'
+        #python python-executor.py '{"func":"storage_getter", "KeyStr":"OmgKey"}'
     exit(0)
 
 
